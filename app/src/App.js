45396ce1922e1df7b3689e259553c31e09fdd8e0
Dylan Box <dylan@dylanbox.com>
2023-04-18 06:53:58 +0000

    Add Orfelia Page

--- app/src/App.js
+++ app/src/App.js
@@ -1,27 +1,31 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // Style
-import 'normalize.css';
-import 'source-code-pro/source-code-pro.css'
-import './app.css';
+import "normalize.css";
+import "source-code-pro/source-code-pro.css";
+import "./app.css";
 
 // Pages
-import Main from './Main.js';
-import Home from './pages/Home.mdx';
-import GenderGenerator from './pages/GenderGenerator.mdx';
-
+import MainLayout from "./MainLayout.js";
+import HighlightLayout from "./HighlightLayout.js";
+import Home from "./pages/Home.mdx";
+import GenderGenerator from "./pages/GenderGenerator.mdx";
+import Orfelia from "./pages/Orfelia/OrfeliaPage.js";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />}>
+        <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
           <Route path="/projects">
             <Route path="gender-generator" element={<GenderGenerator />} />
           </Route>
         </Route>
+        <Route path="/" element={<HighlightLayout />}>
+          <Route path="orfelia" element={<Orfelia />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
