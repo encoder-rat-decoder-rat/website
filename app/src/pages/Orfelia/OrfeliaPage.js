a31c2d9f2da379ac54bd26b4b54630837105338a
Dylan Box <dylan@dylanbox.com>
2023-04-26 06:19:18 +0000

    Add Image to Orfelia Page

--- app/src/pages/Orfelia/OrfeliaPage.js
+++ app/src/pages/Orfelia/OrfeliaPage.js
@@ -1,10 +1,15 @@
 import React from "react";
 import OrfeliaContent from "./OrfeliaContent.mdx";
+import image from "../../images/orfelia-slide-1.jpeg";
 
 const OrfeliaPage = () => {
   return (
     <div id="orfelia-page">
       <section className="container">
+        <img
+          src={image}
+          alt="Queer Futures Presents: A Night at the Orfelia. Detroit 2023. Produced By Encoder Rat Decoder Rat. The future is not yet queer."
+        />
         <OrfeliaContent />
       </section>
     </div>
