dece5a2e0795d571422cacde072bf839d3470ead
Dylan Box <487275+dylanbox@users.noreply.github.com>
2024-12-19 01:48:16 -0500

    Update: Pre-render react routes

--- app/react-router.config.ts
+++ app/react-router.config.ts
@@ -2,9 +2,9 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   appDirectory: "src",
-  ssr: false,
+  ssr: true,
   // return a list of URLs to prerender at build time
   async prerender() {
-    return [];
+    return ["/", "/orfelia", "/orfelia/about", "/orfelia/gallery", "/orfelia/splunk", "/orfelia/messsy"];
   },
 } satisfies Config;
